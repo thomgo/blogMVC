e66310ca05c38c9a9812f495fc5fa24f732af457
Thomas <tg.59290@gmail.com>
2018-01-28 13:27:27 +0100

    took off db credancials and added beggining off an htaccess file

--- model/database/dbconnection.php
+++ model/database/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 function getPDO() {
   try {
-      $bdd = new PDO('mysql:host=localhost;dbname=simplon_php;charset=utf8', 'root', 'ThomAdmin12');
+      $bdd = new PDO('mysql:host=localhost;dbname=;charset=utf8', '', '');
   }
   catch (Exception $error) {
           die('Erreur : ' . $error->getMessage());
