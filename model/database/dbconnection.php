46c3eaabbc1e992819bb2c15e254f06563b8f3c6
Thomas <tg.59290@gmail.com>
2019-05-16 11:17:35 +0200

    update comment in database

--- model/database/dbconnection.php
+++ model/database/dbconnection.php
@@ -1,9 +1,9 @@
 <?php
-// Function to return an instance of PDO object conencted to your database
+// Function to return an instance of PDO object connected to your database
 // For now each call to the function returns a new instance, later you'll learn to do better
 function getPDO() {
   try {
-      $bdd = new PDO('mysql:host=localhost;dbname=simplon_php;charset=utf8', 'root', 'ThomAdmin12');
+      $bdd = new PDO('mysql:host=localhost;dbname=ecommerce;charset=utf8', 'root', 'ThomAdmin12');
   }
   catch (Exception $error) {
           die('Erreur : ' . $error->getMessage());
